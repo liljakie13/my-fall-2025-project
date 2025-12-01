770f3e04f7b155897cf3620c1a5c2de211463280
Jacob Nieves <80655371+liljakie13@users.noreply.github.com>
2025-12-01 09:49:33 -0500

    Revert "merge"

--- MypythonWebApp/app/routes.py
+++ MypythonWebApp/app/routes.py
@@ -43,10 +43,13 @@ def registration():
        print("Hi!")  ## erase this because it just there to prevent error
     return render_template("registration.html") ## handle the registration form submission  
 
-@bp.route("/feedback", methods=["GET", "POST"]) ##
+@bp.route("/feedback", methods=["GET", "POST"]) ## localhost/register
 def feedback():
-    return render_template("feedback.html") ##
+    """
+    this is the route for the feedback page
+    """
+    if request.method == "POST":
+       ## register.py script logic here
+       print("Hi!")  ## erase this because it just there to prevent error
+    return render_template("feedback.html") ## handle the registration form submission  
 
-@bp.route("/menu", methods=["GET", "POST"]) ##
-def menu():
-    return render_template("menu.html") ##
